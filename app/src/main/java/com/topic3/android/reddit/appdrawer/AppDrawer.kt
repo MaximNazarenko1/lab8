ba5e786bfdc9ee320740ca6362fbaedce86850e4
Zver <dima.852.dma@gmail.com>
2023-05-29 17:13:58 +0300

    change AppDrawer(ScreenNavigationButton,AppDrawerFooter)

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -2,6 +2,7 @@ package com.topic3.android.reddit.appdrawer
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -15,6 +16,7 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -253,11 +255,102 @@ private fun ScreenNavigationButton(
 /**
  * Представляет компонент настройки в панели приложений.
  */
+
+@Composable
+private fun ScreenNavigationButton(
+  icon: ImageVector,
+  label: String,
+  onClickAction: () -> Unit,
+  modifier: Modifier = Modifier
+) {
+  val colors = MaterialTheme.colors
+
+  val surfaceModifier = modifier
+    .padding(start = 8.dp, top = 8.dp, end = 8.dp)
+    .fillMaxWidth()
+
+  Surface(
+    modifier = surfaceModifier,
+    color = colors.surface,
+    shape = MaterialTheme.shapes.small
+  ) {
+    TextButton(
+      onClick = onClickAction,
+      modifier = Modifier.fillMaxWidth()
+    ) {
+      Row(
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+      ) {
+        Image(
+          imageVector = icon,
+          colorFilter = ColorFilter.tint(Color.Gray),
+          contentDescription = label
+        )
+        Spacer(Modifier.width(16.dp))
+        Text(
+          fontSize = 10.sp,
+          text = label,
+          style = MaterialTheme.typography.body2,
+          color = colors.primaryVariant
+        )
+      }
+    }
+  }
+}
+
 @Composable
 private fun AppDrawerFooter(modifier: Modifier = Modifier) {
   //TODO add your code here
-}
+  ConstraintLayout(
+    modifier = modifier
+      .fillMaxSize()
+      .padding(
+        start = 16.dp,
+        bottom = 16.dp,
+        end = 16.dp
+      )
 
+  ) {
+    val colors = MaterialTheme.colors
+    val (settingsImage, settingsText, darkModeButton) = createRefs()
+    Icon(
+      modifier = modifier.constrainAs(settingsImage){
+        start.linkTo(parent.start)
+        bottom.linkTo(parent.bottom)
+      },
+      imageVector = Icons.Default.Settings,
+      contentDescription = stringResource(R.string.settings
+      ),
+      tint = colors.primaryVariant
+
+    )
+    Text(
+      fontSize = 10.sp,
+      text = stringResource(R.string.settings),
+      style = MaterialTheme.typography.body2,
+      color = colors.primaryVariant,
+      modifier = modifier
+        .padding(start = 16.dp)
+        .constrainAs(settingsText) {
+          start.linkTo(settingsImage.end)
+          centerVerticallyTo(settingsImage)
+        }
+    )
+    Icon(
+      imageVector = ImageVector.vectorResource(id = R.drawable.ic_moon),
+      contentDescription = stringResource(id = R.string.change_theme),
+      modifier = modifier
+        .clickable(onClick = { changeTheme() })
+        .constrainAs(darkModeButton) {
+          end.linkTo(parent.end)
+          bottom.linkTo(settingsImage.bottom)
+        },
+      tint = colors.primaryVariant
+    )
+  }
+}
 private fun changeTheme() {
   RedditThemeSettings.isInDarkTheme.value = RedditThemeSettings.isInDarkTheme.value.not()
 }
